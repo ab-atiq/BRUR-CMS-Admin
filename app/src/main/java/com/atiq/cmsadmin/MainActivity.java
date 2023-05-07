bc8df12c7e744aedb49f43f9369092493981536d
atiqur rahman <abulbasharatiqur@gmail.com>
2023-05-07 14:34:03 +0600

    Task2 without connect Firebase

--- app/src/main/java/com/atiq/cmsadmin/MainActivity.java
+++ app/src/main/java/com/atiq/cmsadmin/MainActivity.java
@@ -2,13 +2,25 @@ package com.atiq.cmsadmin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.card.MaterialCardView;
 
 public class MainActivity extends AppCompatActivity {
-
+    MaterialCardView addNotice;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addNotice = findViewById(R.id.addNotice);
+
+        addNotice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),AddNoticeActivity.class));
+            }
+        });
     }
 }
